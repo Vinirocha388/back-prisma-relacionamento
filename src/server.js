cb6e0ade0d57369bb95561046a22abff564cb7cb
Vinícius Rocha <vinicius.a.rocha8@aluno.senai.br>
2025-04-29 09:34:06 -0300

    feat: create user route

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import collectionRoutes from "./routes/collectionRoutes.js";
 import cardsRoutes from "./routes/cardsRoutes.js";
+import authRoutes from "./routes/auth.routes.js"; // importar as rotas de autenticação
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 4001;
 app.use(express.json());
 app.use("/colecoes", collectionRoutes);
 app.use("/cards", cardsRoutes);
+app.use("/auth", authRoutes);// usar as rotas de autenticação
 app.listen(port, () => {
   console.log(`Servidor rodando na porta: ${port}`);
 });
